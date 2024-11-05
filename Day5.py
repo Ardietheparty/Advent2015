091c4d3a18021620a8b5669d0f60f8fa142a36e4
Ardietheparty <94144298+Ardietheparty@users.noreply.github.com>
2024-11-04 21:28:58 -0700

    Update Day5.py

--- Day5.py
+++ Day5.py
@@ -1,7 +1,6 @@
 import os
 from platform import system
 
-
 def testa(str):
     nums = 0
     for char in str:
@@ -25,13 +24,34 @@ def testc(str):
             print(str[i], str[i+1])
             return False
     return True
+
+
+def testaa(str):
+    for i in range(len(str) - 1):
+        hld = str[i] + str[i+1]
+        j = i + 2
+        while j + 2 < len(str):
+            hld2 = str[j] + str[j + 1]
+            if hld == hld2:
+                return True
+            j+=1
+    return False
+
+def testbb(str):
+    for i in range(len(str) - 2):
+        if str[i] == str[i+2]:
+            return True
+    return False
+
 numofstrings = 0
 with open('Day5in', 'r') as f:
     for line in f:
-        if testa(line) and testb(line) and testc(line):
+        #if testa(line) and testb(line) and testc(line):
             numofstrings += 1
-
-
-
-
+print(numofstrings)
+numofstrings = 0
+with open('Day5in', 'r') as f:
+    for line in f:
+        if testaa(line) and testbb(line):
+            numofstrings += 1
 print(numofstrings)
