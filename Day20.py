1aeed9eca1ada2b12f9a7142ab91767d3c8b181d
Ardietheparty <94144298+Ardietheparty@users.noreply.github.com>
2024-11-08 12:21:11 -0700

    Update Day20.py

--- Day20.py
+++ Day20.py
@@ -1,12 +1,15 @@
-ini = 2900000
+ini = 29000000
+nini = int ( ini / 10)
 houses = [0]*ini
 test = True
 r=0
-for i in range(1, ini):
-    for j in range(i, ini, i):
-        houses[j]+=j
+temp = False
+for i in range(1, nini):
+    for j in range(i, nini, i):
+        houses[j]+=i * 10
 
-for i in range(ini):
-    if houses[i] == ini:
+for i in range(nini):
+    if houses[i] >= ini:
         print(i)
         break
+
