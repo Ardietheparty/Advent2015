e363cd8fc567cecd09c93a42db5c924ea8aed775
Ardietheparty <94144298+Ardietheparty@users.noreply.github.com>
2024-11-09 18:14:47 -0700

    Update Day20.py

--- Day20.py
+++ Day20.py
@@ -12,4 +12,19 @@ for i in range(nini):
     if houses[i] >= ini:
         print(i)
         break
-
+house = [0]*51
+while test:
+    elf = 1
+    for j in range(1, nini):
+        for i in range(1, 51, j):
+            house[i] += j * 11
+            if house[i] >= ini:
+                #print(i)
+                test = False
+                break
+    if test:
+        break
+for i in range(1, 51):
+    if house[i] >= ini:
+        print(i)
+        break
